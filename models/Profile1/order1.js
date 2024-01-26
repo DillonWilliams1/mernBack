0a1b060d330c4507e527ea8839a2a3fafefdff17
DillonWilliams1 <136365918+DillonWilliams1@users.noreply.github.com>
2024-01-26 13:29:49 +0530

    order1.js

--- models/Profile1/order1.js
+++ models/Profile1/order1.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
 
-const order1Schema = new mongoose.Schema({
-  name: String,
-  CustomerName: String,
+const orderSchema = new mongoose.Schema({
+  items: [{ name: String, price: Number }],
   price: Number,
-  Quantity: Number,
-  CustomerID: String,
-  CustomerPhoneNo: String
 });
-
-const order1Model = mongoose.model('order1', order1Schema);
-
+const Order = mongoose.model('Order', orderSchema);
 
 
-module.exports = order1Model;
 
+module.exports = Order;
