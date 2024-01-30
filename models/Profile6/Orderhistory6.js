c5ffaee2cd0239bd3cc3d475d41c03ece21f0266
DillonWilliams1 <136365918+DillonWilliams1@users.noreply.github.com>
2024-01-30 22:26:26 +0530

    Add files via upload

---
+++ models/Profile6/Orderhistory6.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    orderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order6',
+      },
+      timestamp: { type: Date, default: Date.now },
+      // Add any additional fields you want to store for accepted order
+      items: [
+        {
+          name: String,
+          price: Number,
+          quantity: Number,
+        },
+      ],
+      price: Number,
+      name: String,
+      phone: String,
+      id: String,
+    });
+
+const Orderhistory = mongoose.model('Orderhistory6', orderSchema);
+
+module.exports = Orderhistory;
