b45c222cd0748db7de3c5784888f17407a90f2b8
DillonWilliams1 <136365918+DillonWilliams1@users.noreply.github.com>
2024-01-30 22:24:10 +0530

    Add files via upload

---
+++ models/Profile4/Order4.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  items: [{name: String,price: Number,quantity: Number,},],
+  price: Number,
+  name: String,
+  phone: String, 
+  id: String,
+  token: String,    
+});
+
+const Order = mongoose.model('Order4', orderSchema);
+
+module.exports = Order;
